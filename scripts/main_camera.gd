332a976292086e9bf83ea26ef0637f5b1963b121
Richard Rodriguez <rmjuarez12@gmail.com>
2025-07-07 22:34:53 -0400

    Remove print

--- scripts/main_camera.gd
+++ scripts/main_camera.gd
@@ -5,6 +5,5 @@ extends Camera2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	print(follow_target)
 	if follow_target:
 		position = follow_target.position
